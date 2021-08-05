27ceb7e90ae58f2f709e5fb3a2423bc124b1f380
Cainã <moaraadrean@gmail.com>
2021-08-04 22:09:51 -0300

    Adicionando arquivos do projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,40 @@
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
+
+export default function Home() {
+  return (
+    <Flex
+      w="100vw"
+      h="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth="360px"
+        bg="gray.800"
+        p="8"
+        borderRadius="8px"
+        flexDirection="column"
+      >
+        <Stack spacing="4">
+          <Input
+            name="email"
+            label="E-mail"
+            type="email"
+          />
+
+          <Input
+            name="password"
+            label="Password"
+            type="password"
+          />
+
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+      </Flex>
+    </Flex>
+  )
+}
